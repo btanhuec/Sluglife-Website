e255a5e549e3dbf88f9e15283382e487f06f5565
Joshua  Tanhueco <BrixNation@Joshuas-MacBook-Pro-7.local>
2019-07-20 11:34:11 -0700

    working on about us

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get "posts/aboutus"
+
   devise_for :users, controllers: {
     registrations: 'registrations'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
+
+
 
   ####root creates a landing page, whenever we edit routes, we should restart server
   root "posts#index"
